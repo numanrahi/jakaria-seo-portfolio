92bbd02c426b5c2c449353182028117862af6d4e
numanrahi <arahi2994@gmail.com>
2023-10-11 23:19:55 +0600

    skills lottie file added

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,69 +1,123 @@
 import ProgressBar from "@ramonak/react-progress-bar";
 import React from "react";
+import Lottie from "react-lottie-player";
 
-import { DiGoogleAnalytics } from "react-icons/di";
-import { AiFillGithub, AiFillHtml5 } from "react-icons/ai";
-import {BsBootstrapFill, BsBugFill} from "react-icons/bs"
-import { TbSeo } from "react-icons/tb";
-import { GrReactjs } from "react-icons/gr";
-import { TbBrandReactNative } from "react-icons/tb";
-import { SiDaisyui, SiExpress, SiFirebase, SiVercel } from "react-icons/si";
-import { BiLogoFigma, BiLogoJavascript, BiLogoMongodb, BiLogoNetlify, BiLogoNodejs, BiLogoTailwindCss } from "react-icons/bi";
+import lottieJson from "../../../public/animation_lnm05ws9.json";
 const Skills = () => {
   return (
     <div>
       <div className="display-4 text-center mb-5 pt-3">My Skills</div>
       <div className="container pt-5 ">
-      <div className="fs-2 text-center fw-bold">Skills</div>
-      <div className="row text-center pt-5">
-        <div className="col-sm-2 col-4 fs-5">
-         Digital Marketing
-        </div>
-        <div className="col-sm-10 col-8">
-          <ProgressBar completed={90} />
-        </div>
+        <div className="fs-2 text-center fw-bold">Skills</div>
+        <section>
+          <Lottie
+          className="mx-auto lottie"
+            loop
+            animationData={lottieJson}
+            play
+          />
+        </section>
+        <section className="fs-5">
+          <div className="d-flex align-items-center text-center pt-5">
+            <div className="col-sm-2 col-4 fs-5">Digital Marketing</div>
+            <div className="col-sm-10 col-8">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">SEO</div>
+            <div className="col-sm-10 col-8">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">SEM</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4">SMM</div>
+            <div className="col-sm-10 col-8">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Web Analytics</div>
+            <div className="col-sm-10 col-8">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Content Strategy</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Copywriting</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Email Campaigns</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">
+              Conversion Rate Optimization
+            </div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Social Paid Advertising</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Writing & Editing</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Excel</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Google Ad words</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">HTML</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">CSS</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+          <div className="d-flex align-items-center text-center py-2">
+            <div className="col-sm-2 col-4 fs-5">Influencer Marketing</div>
+            <div className="col-sm-10 col-8 fs-5">
+              <ProgressBar completed={90} />
+            </div>
+          </div>
+        </section>
       </div>
-      <div className="row text-center py-2">
-        <div className="col-sm-2 col-4 fs-5">
-         SEO
-        </div>
-        <div className="col-sm-10 col-8">
-          <ProgressBar completed={90} />
-        </div>
-      </div>
-      <div className="row text-center py-2">
-        <div className="col-sm-2 col-4 fs-5">
-        SEM
-        </div>
-        <div className="col-sm-10 col-8 fs-5">
-          <ProgressBar completed={90} />
-        </div>
-      </div>
-      <div className="row text-center py-2 fs-5">
-        <div className="col-sm-2 col-4">
-        SMM
-        </div>
-        <div className="col-sm-10 col-8">
-          <ProgressBar completed={90} />
-        </div>
-      </div>
-      <div className="row text-center py-2">
-        <div className="col-sm-2 col-4 fs-5">
-        Web Analytics
-        </div>
-        <div className="col-sm-10 col-8">
-          <ProgressBar completed={90} />
-        </div>
-      </div>
-      <div className="row text-center py-2">
-        <div className="col-sm-2 col-4 fs-5">
-        Content Strategy
-        </div>
-        <div className="col-sm-10 col-8 fs-5">
-          <ProgressBar completed={90} />
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
